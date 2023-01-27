890a6d9bc4d0ce10a837925524a3393731ae107c
Jarupat <122600994+6513161@users.noreply.github.com>
2023-01-27 10:52:55 +0000

    bubble very funny

--- .ccls-cache/@home@runner@bubble-sort-6513161/main.c
+++ .ccls-cache/@home@runner@bubble-sort-6513161/main.c
@@ -1,19 +1,45 @@
-#define N 6
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "sorting.h"
 
-int main() {
-  int a[N]={3,2,6,7,3,1};
-  int i,j,new_number;
+int Isprime(int n)
+{
+   if(n==1) 
+     return 0;
+  for(int i=2; i<n; i++)
+    { 
+      if(n%i==0)
+        return 0;
+    }
+  return 1;
+}
 
-   display(a,N);
-  bubbleSort(a,N); 
+int main(int argc, char *argv[]) 
+{
+  int *a,n,j=0,i,*prime;
+  n=argc-1;
+  a=(int*)malloc(sizeof(int)*n);
+  prime=(int*)malloc(sizeof(int)*n);
+  for(i=0;i<n;i++)
+    {
+      a[i]=atoi(argv[i+1]);
+      if(Isprime(a[i]))
+      {
+        prime[j]=a[i];
+        j++;
+      }
+    }
+  n=j;
+  
+  if(n==0) printf("No prime\n");
+  else{
+   display(prime,n);
+  bubbleSort(prime,n); 
    //insertion(a,N);
 
   // selectionSort(a,N);
-   display(a,N);
- return 0;
+   display(prime,n);
+    }
+    return 0;
+  
 }
-
-
